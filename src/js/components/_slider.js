39d09e4b0ced62b29301e5e3dde1dcea44bcfed9
TaVitka <tavitka17@gmail.com>
2024-03-27 21:36:19 +0200

    added offices, saunas, sheds sections for services page

--- src/js/components/_slider.js
+++ src/js/components/_slider.js
@@ -8,19 +8,19 @@ let options = {
   autoplay: {
     delay: 200,
   },
-  // centeredSlides: true,
   modules: [Navigation, Scrollbar, Pagination, EffectFade],
   draggable: true,
   grabCursor: true,
   pagination: {
     el: '.swiper-pagination',
+    clickable: true
   }
 }
 
 const services1 = new Swiper('.services__slider1', options);
-// const gallery2 = new Swiper('.gallery__slider2', options);
-// const gallery3 = new Swiper('.gallery__slider3', options);
-// const gallery4 = new Swiper('.gallery__slider4', options);
+const services2 = new Swiper('.services__slider2', options);
+const services3 = new Swiper('.services__slider3', options);
+
 
 const swiperReviews = new Swiper('.reviews__slider', {
   slidesPerView: 1,
