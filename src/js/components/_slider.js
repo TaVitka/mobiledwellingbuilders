dacb3e2aac7b477f7f43b7f6e4f35f9594e94fba
TaVitka <tavitka17@gmail.com>
2024-03-27 21:20:36 +0200

    added homes section for services page

--- src/js/components/_slider.js
+++ src/js/components/_slider.js
@@ -1,5 +1,27 @@
 import Swiper, { Navigation, Pagination, EffectFade, Scrollbar } from 'swiper';
 
+let options = {
+  slidesPerView: 1,
+  speed: 700,
+  gap: 20,
+  autoHeight: true,
+  autoplay: {
+    delay: 200,
+  },
+  // centeredSlides: true,
+  modules: [Navigation, Scrollbar, Pagination, EffectFade],
+  draggable: true,
+  grabCursor: true,
+  pagination: {
+    el: '.swiper-pagination',
+  }
+}
+
+const services1 = new Swiper('.services__slider1', options);
+// const gallery2 = new Swiper('.gallery__slider2', options);
+// const gallery3 = new Swiper('.gallery__slider3', options);
+// const gallery4 = new Swiper('.gallery__slider4', options);
+
 const swiperReviews = new Swiper('.reviews__slider', {
   slidesPerView: 1,
   speed: 700,
